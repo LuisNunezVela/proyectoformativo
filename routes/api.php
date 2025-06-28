95c05b0daa7e33cb502df7ce3d2c868865397d84
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-06-28 07:59:44 -0400

    added imports

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->group(function () {
